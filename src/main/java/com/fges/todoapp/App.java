f6483fd46bc1ede06e0d7695cd12aa5e3996e6ad
FrancescoAbouNader <francescoabounader@gmail.com>
2024-02-24 19:59:35 +0100

    Hello

--- src/main/java/com/fges/todoapp/App.java
+++ src/main/java/com/fges/todoapp/App.java
@@ -1,9 +1,23 @@
 package com.fges.todoapp;
 
-import java.nio.file.*;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.exit(new TodoApp().exec(args));
+        CLIOptions cliOptions = new CLIOptions(args);
+        if (!cliOptions.parse()) {
+            System.exit(1);
+        }
+
+        CommandHandler handler = CommandHandlerFactory.getHandler(cliOptions.getCommand(), cliOptions.getFileName());
+        if (handler == null) {
+            System.err.println("Unsupported command");
+            System.exit(1);
+        }
+
+        handler.execute(cliOptions.getPositionalArgs());
+    }
+
+    public static int exec(String[] array) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'exec'");
     }
 }
